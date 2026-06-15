4f1003c2d6105e8607b3dd546edfb8be9cf7f3eb
xth3ory <davidjcox1993@gmail.com>
2026-06-15 12:50:20 -0400

    chart update

--- sampleDash.py
+++ sampleDash.py
@@ -2,8 +2,11 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
+chart_data_1 = pd.read_csv('inbound.csv')
+chart_data_2 = pd.read_csv('outbound.csv')
 
-st.line_chart(chart_data)
+chart_data_1['month'] = pd.to_datetime(chart_data_1['month'])
+chart_data_2['month'] = pd.to_datetime(chart_data_2['month'])
+
+st.line_chart(chart_data_1, x='month', y='Inbound_Transaction_Count')
+st.line_chart(chart_data_2, x='month', y='Outbound_Transaction_Count')
