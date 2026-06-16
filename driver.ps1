2a14a37032bf14c7a06eb079b10cd260cd77335f
David Cox <dcox@srta.ga.gov>
2026-06-16 05:28:15 -0400

    made changes

--- driver.ps1
+++ driver.ps1
@@ -1,5 +1,7 @@
 Set-Location "C:\Users\dcox\Projects\Posit Sample"
+Write-Output "Loading python env"
 C:\PythonEnv\monitoring\Scripts\Activate.ps1
+Write-Output "Running script"
 python updateData.py | Out-File -FilePath script.log 
 git add *.csv
 git commit -am "made changes"
